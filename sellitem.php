e3ba3c546810356ee293ff5817c0c5948117b3c6
vishrut <vrp101@gmail.com>
2013-03-25 11:23:35 +0530

    Beginning with Add-item interface

---
+++ sellitem.php
@@ -0,0 +1,67 @@
+﻿<!DOCTYPE html>
+<?php session_start() ?>
+<html lang="en">
+  <head>
+	<script src="validation.js"></script>
+	<meta charset="utf-8">
+    <title>Posh</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Le styles -->
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+    <style type="text/css">
+		body {
+			padding-top: 40px;
+			padding-bottom: 40px;
+		}
+
+    </style>
+    <link href="bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+	</head>
+
+	<body>
+	
+		<div class="navbar navbar-fixed-top">
+			<div class="navbar-inner">
+				<div class="container" style="width: auto; padding: 0 20px;">
+					<a class="brand" href="#">Posh</a>
+					<ul class="nav pull-right">
+						<li><a href="#">Search</a></li>
+						<li><a href="#">What I'm Buying</a></li>
+						<li class="active"><a href="#">Sell an Item</a></li>
+						<li><a href="#">Help Center</a></li>
+						<li><a href="#">My Account</a></li>
+						<li><a href="#">Log Out</a></li>
+					</ul>
+				</div>
+			</div>	
+		</div>
+		
+		<div class="container" style="width: auto; padding: 20px 0px 0px 0px;">
+			<div class="span3">
+			<ul class="nav nav-tabs nav-stacked">
+              <li class="active"><a href="#">Add Item</a></li>
+              <li><a href="#">Items I'm Selling</a></li>
+              <li><a href="#">My Services</a></li>
+            </ul>
+			</div>
+			<div class="span9">
+				<h1>Hello</h1>
+			</div>
+		</div>
+
+
+
+		<hr>
+
+		<div class="footer" style="width: auto; padding: 0px 0px 0px 20px;">
+			<p>&copy; Company 2012</p>
+		</div>
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="bootstrap/js/jquery.js"></script>
+    <script src="bootstrap/js/bootstrap.js"></script>
+  </body>
+</html>
