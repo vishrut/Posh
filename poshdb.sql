fc1b8a1a435ba760d7162774a937fc4838b9c77c
vishrut <vrp101@gmail.com>
2013-04-11 01:14:01 +0530

    new sql file

--- poshdb.sql
+++ poshdb.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 10, 2013 at 11:14 AM
+-- Generation Time: Apr 10, 2013 at 07:43 PM
 -- Server version: 5.5.27
 -- PHP Version: 5.3.4
 
@@ -34,12 +34,15 @@ CREATE TABLE IF NOT EXISTS `comments` (
   PRIMARY KEY (`commentid`),
   KEY `commenter` (`commenter`),
   KEY `offerid` (`offerid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=49 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=53 ;
 
 --
 -- Dumping data for table `comments`
 --
 
+INSERT INTO `comments` (`commentid`, `commenter`, `offerid`, `time`, `comment`) VALUES
+(50, 'vishrut', 73, '2013-04-10 23:14:45', 'hi how are you?'),
+(52, 'vishrut', 76, '2013-04-10 23:15:11', 'whats your name?');
 
 -- --------------------------------------------------------
 
@@ -77,7 +80,7 @@ CREATE TABLE IF NOT EXISTS `inventory` (
   `image` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`itemid`),
   KEY `owner` (`owner`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=29 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=30 ;
 
 --
 -- Dumping data for table `inventory`
@@ -87,7 +90,8 @@ INSERT INTO `inventory` (`itemid`, `owner`, `itemname`, `category`, `addedon`, `
 (15, 'vi', 'itemvi', 'A', '2013-03-26 18:11:01', 100, 'hellos							', 'new', '627102ERD.png'),
 (25, 'saheb', 'sahebsitem', 'A', '2013-04-08 00:33:56', 1000, 'This is sahebs item							', 'good', '270413Level1.png'),
 (27, 'saheb', 'noti', 'A', '2013-04-10 11:22:33', 25, 'this is an aitem							', 'new', '806164ERD.png'),
-(28, 'saheb', 'hudi', 'A', '2013-04-10 12:25:08', 33, '							wadup', 'new', '874908ERD.png');
+(28, 'saheb', 'hudi', 'A', '2013-04-10 12:25:08', 33, '							wadup', 'new', '874908ERD.png'),
+(29, 'vishrut', 'vishrutsitem', 'A', '2013-04-10 22:52:21', 4500, 'hi							', 'new', '730548ERD.png');
 
 -- --------------------------------------------------------
 
@@ -103,7 +107,7 @@ CREATE TABLE IF NOT EXISTS `notifications` (
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`notificationid`),
   KEY `username` (`username`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=40 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=58 ;
 
 --
 -- Dumping data for table `notifications`
@@ -120,11 +124,29 @@ INSERT INTO `notifications` (`notificationid`, `notificationdetails`, `username`
 (27, 'Transaction status update!<br>Transaction between <b>vish and vish</b><br>User <b>vish</b> has reported the transaction as unsuccessful. <br>User <b>vi</b> has reported the transaction as completed', 'vi', 0, '2013-04-10 12:54:07'),
 (30, 'Transaction status update!<br>Transaction between <b>vish and vi</b><br>Both users have reported the transaction as completed', 'vi', 0, '2013-04-10 12:55:22'),
 (31, 'Transaction status update!<br>Transaction between <b>saheb and vish</b><br>User <b>vish</b> has reported the transaction as completed', 'saheb', 1, '2013-04-10 12:56:31'),
-(33, 'User <b>vish</b> has made an offer for your item <b>hudi</b>', 'saheb', 0, '2013-04-10 14:05:32'),
-(34, 'Transaction status update!<br>Transaction between <b>saheb and vish</b><br>User <b>vish</b> has reported the transaction as completed', 'saheb', 0, '2013-04-10 14:11:38'),
-(36, 'Transaction status update!<br>Transaction between <b>saheb and vish</b><br>User <b>vish</b> has reported the transaction as completed', 'saheb', 0, '2013-04-10 14:11:43'),
+(33, 'User <b>vish</b> has made an offer for your item <b>hudi</b>', 'saheb', 1, '2013-04-10 14:05:32'),
+(34, 'Transaction status update!<br>Transaction between <b>saheb and vish</b><br>User <b>vish</b> has reported the transaction as completed', 'saheb', 1, '2013-04-10 14:11:38'),
+(36, 'Transaction status update!<br>Transaction between <b>saheb and vish</b><br>User <b>vish</b> has reported the transaction as completed', 'saheb', 1, '2013-04-10 14:11:43'),
 (38, 'User <b>saheb</b> has made an offer for your item <b>itemvi</b>', 'vi', 0, '2013-04-10 16:02:40'),
-(39, 'User <b>saheb</b> has made an offer for your item <b>itemvi</b>', 'vi', 0, '2013-04-10 16:12:39');
+(39, 'User <b>saheb</b> has made an offer for your item <b>itemvi</b>', 'vi', 0, '2013-04-10 16:12:39'),
+(40, 'User <b>vishrut</b> has made an offer for your item <b>hudi</b>', 'saheb', 1, '2013-04-10 22:51:27'),
+(41, 'User <b>vishrut</b> has made an offer for your item <b>sahebsitem</b>', 'saheb', 1, '2013-04-10 22:51:54'),
+(42, 'User <b>vishrut</b> has made an offer for your item <b>sahebsitem</b>', 'saheb', 1, '2013-04-10 22:52:41'),
+(43, 'User <b>vishrut</b> has retracted an offer for your item <b>sahebsitem</b>', 'saheb', 1, '2013-04-10 22:59:09'),
+(44, 'User <b>vishrut</b> has made an offer for your item <b>sahebsitem</b>', 'saheb', 1, '2013-04-10 22:59:35'),
+(45, 'User <b>saheb</b> has made an offer for your item <b>vishrutsitem</b>', 'vishrut', 1, '2013-04-10 23:06:34'),
+(46, 'Congratulations! User <b>vishrut</b> has accepted your offer for the item <b>vishrutsitem</b>.<br>You can find your new transaction in the Offers and Transactions tab.<br>Seller username: vishrut<br>Seller name: Vishrut Patel<br>Room number: F 117<br>Email: vrp101@gmail.com', 'saheb', 1, '2013-04-10 23:21:00'),
+(47, 'Congratulations! You have entered a transaction with <b>vishrut</b> for the item <b>vishrutsitem</b>.<br>You can find your new transaction in the Offers and Transactions tab.<br>Buyer username: saheb<br>Buyer name: Saheb Motiani<br>Room number: A 123<br>Email: 201001158@daiict.ac.in', 'vishrut', 1, '2013-04-10 23:21:00'),
+(48, 'Transaction status update!<br>Transaction between <b>saheb and vishrut</b><br>User <b>vishrut</b> has reported the transaction as completed', 'saheb', 1, '2013-04-10 23:31:30'),
+(49, 'Transaction status update!<br>Transaction between <b>saheb and vishrut</b><br>User <b>vishrut</b> has reported the transaction as completed', 'vishrut', 1, '2013-04-10 23:31:30'),
+(50, 'Transaction status update!<br>Transaction between <b>saheb and vishrut</b><br>Both users have reported the transaction as completed', 'saheb', 1, '2013-04-10 23:31:54'),
+(51, 'Transaction status update!<br>Transaction between <b>saheb and vishrut</b><br>Both users have reported the transaction as completed', 'vishrut', 1, '2013-04-10 23:31:54'),
+(52, 'Congratulations! User <b>saheb</b> has accepted your offer for the item <b>sahebsitem</b>.<br>You can find your new transaction in the Offers and Transactions tab.<br>Seller username: saheb<br>Seller name: Saheb Motiani<br>Room number: A 123<br>Email: 201001158@daiict.ac.in', 'vishrut', 0, '2013-04-11 00:01:32'),
+(53, 'Congratulations! You have entered a transaction with <b>saheb</b> for the item <b>sahebsitem</b>.<br>You can find your new transaction in the Offers and Transactions tab.<br>Buyer username: vishrut<br>Buyer name: Vishrut Patel<br>Room number: F 117<br>Email: vrp101@gmail.com', 'saheb', 0, '2013-04-11 00:01:32'),
+(54, 'Transaction status update!<br>Transaction between <b>vishrut and saheb</b><br>User <b>saheb</b> has reported the transaction as completed', 'vishrut', 0, '2013-04-11 00:02:14'),
+(55, 'Transaction status update!<br>Transaction between <b>vishrut and saheb</b><br>User <b>saheb</b> has reported the transaction as completed', 'saheb', 0, '2013-04-11 00:02:14'),
+(56, 'Transaction status update!<br>Transaction between <b>vishrut and saheb</b><br>User <b>vishrut</b> has reported the transaction as unsuccessful. <br>User <b>saheb</b> has reported the transaction as completed', 'vishrut', 0, '2013-04-11 00:02:23'),
+(57, 'Transaction status update!<br>Transaction between <b>vishrut and saheb</b><br>User <b>vishrut</b> has reported the transaction as unsuccessful. <br>User <b>saheb</b> has reported the transaction as completed', 'saheb', 0, '2013-04-11 00:02:23');
 
 -- --------------------------------------------------------
 
@@ -153,7 +175,7 @@ CREATE TABLE IF NOT EXISTS `offerdetails` (
   KEY `offereditem4` (`offereditem4`),
   KEY `sellingitem` (`sellingitem`),
   KEY `buyer` (`buyer`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=73 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=78 ;
 
 --
 -- Dumping data for table `offerdetails`
@@ -161,7 +183,10 @@ CREATE TABLE IF NOT EXISTS `offerdetails` (
 
 INSERT INTO `offerdetails` (`offerid`, `buyer`, `seller`, `ssr`, `sellingitem`, `offereditem1`, `offereditem2`, `offereditem3`, `offereditem4`, `cash`, `timestamp`, `accepted`) VALUES
 (71, 'saheb', 'vi', 'sell', 15, NULL, NULL, NULL, NULL, 123, '2013-04-10 16:02:39', 0),
-(72, 'saheb', 'vi', 'swap', 15, 27, NULL, NULL, NULL, NULL, '2013-04-10 16:12:39', 0);
+(72, 'saheb', 'vi', 'swap', 15, 27, NULL, NULL, NULL, NULL, '2013-04-10 16:12:39', 0),
+(73, 'vishrut', 'saheb', 'sell', 28, NULL, NULL, NULL, NULL, 1234, '2013-04-10 22:51:27', 0),
+(76, 'vishrut', 'saheb', 'swap', 25, 29, NULL, NULL, NULL, NULL, '2013-04-10 22:59:35', 1),
+(77, 'saheb', 'vishrut', 'sell', 29, NULL, NULL, NULL, NULL, 234, '2013-04-10 23:06:34', 1);
 
 -- --------------------------------------------------------
 
@@ -224,7 +249,8 @@ INSERT INTO `rentdetails` (`offerid`, `startdate`, `enddate`, `amount`, `per`) V
 (47, '04/10/2013', '04/11/2013', 45, 'Day'),
 (62, '04/11/2013', '04/13/2013', 2400, 'Day'),
 (66, '04/10/2013', '04/11/2013', 25, 'Day'),
-(67, '04/11/2013', '04/13/2013', 1, 'Day');
+(67, '04/11/2013', '04/13/2013', 1, 'Day'),
+(74, '04/10/2013', '04/11/2013', 45, 'Day');
 
 -- --------------------------------------------------------
 
@@ -270,7 +296,8 @@ INSERT INTO `ssritem` (`itemid`, `ssr`) VALUES
 (15, 'swap'),
 (15, 'rent'),
 (27, 'sell'),
-(28, 'sell');
+(28, 'sell'),
+(29, 'sell');
 
 -- --------------------------------------------------------
 
@@ -290,6 +317,9 @@ CREATE TABLE IF NOT EXISTS `transaction` (
 -- Dumping data for table `transaction`
 --
 
+INSERT INTO `transaction` (`transactionid`, `status`, `startdate`, `enddate`) VALUES
+(77, 'totalcompleted', '2013-04-10 23:21:00', NULL),
+(76, 'bisc', '2013-04-11 00:01:32', NULL);
 
 -- --------------------------------------------------------
 
@@ -321,12 +351,12 @@ INSERT INTO `users` (`username`, `studentid`, `email`, `firstname`, `lastname`, 
 ('abc', 201001199, 'vrp101@gmail.co.in', 'Vishrut', 'Patel', 117, 'A', 5, 'webmail', 0, 1, NULL),
 ('aman', 201001231, '201001231@daiict.ac.in', 'Aman', 'Jain', 123, 'A', 5, 'amanjain', 1, 1, 13),
 ('Arun', 201001111, 'arungupta@gmail.com', 'Arun ', 'GUpta', 110, 'A', 5, 'arungupta', 0, 1, NULL),
-('saheb', 201001158, '201001158@daiict.ac.in', 'Saheb', 'Motiani', 123, 'A', 3.58334, 'sahebrocks', 1, 12, NULL),
+('saheb', 201001158, '201001158@daiict.ac.in', 'Saheb', 'Motiani', 123, 'A', 3.35715, 'sahebrocks', 1, 14, NULL),
 ('shikhar', 201001103, 'gshikhri@gmail.com', 'shikhar', 'Gupta', 218, 'A', 5, 'qwerty', 0, 1, NULL),
 ('vedang', 200801135, 'vrp101@gmail.coms', 'V', 'P', 219, 'A', 5, 'abcdef', 0, 1, NULL),
 ('vi', 201001194, 'vrp101@gmail.co', 'Vishrut', 'Patel', 117, 'A', 3.66667, 'aaaaaa', 0, 3, NULL),
 ('vish', 201001010, '201001193@daiict.ac.in', 'Vishrut', 'Patel', 117, 'F', 4, 'aaaaaa', 1, 1, NULL),
-('vishrut', 201001193, 'vrp101@gmail.com', 'Vishrut', 'Patel', 117, 'F', 5, 'aaaaaa', 1, 1, NULL),
+('vishrut', 201001193, 'vrp101@gmail.com', 'Vishrut', 'Patel', 117, 'F', 5, 'turhsiv', 1, 3, 620),
 ('vishrutp', 201000000, 'vishrut103@gmail.com', 'Vishrut', 'Patel', 117, 'A', 5, 'abcdef', 0, 1, NULL);
 
 -- --------------------------------------------------------
@@ -346,6 +376,8 @@ CREATE TABLE IF NOT EXISTS `wishlist` (
 -- Dumping data for table `wishlist`
 --
 
+INSERT INTO `wishlist` (`username`, `itemid`) VALUES
+('vishrut', 28);
 
 --
 -- Constraints for dumped tables
